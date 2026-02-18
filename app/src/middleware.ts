31a198f546eea4d46c1fd1d8369b5e0d720308e9
Denner Azevedo <dennergazevedo@gmail.com>
2026-02-18 15:47:38 -0300

    fix: remove middleware comment

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -5,7 +5,6 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get("auth_token");
   const { pathname } = req.nextUrl;
 
-  // Se não estiver logado e não estiver na rota de auth → redireciona
   if (!token && !pathname.startsWith("/auth")) {
     const loginUrl = new URL("/auth", req.url);
     return NextResponse.redirect(loginUrl);
