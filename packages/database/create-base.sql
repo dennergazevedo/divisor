095f00c1080278ba8810519c0225e6637dd548ef
Denner Azevedo <dennergazevedo@gmail.com>
2026-02-20 23:27:40 -0300

    feat(db): add index for stripe_id in invoice table

--- packages/database/create-base.sql
+++ packages/database/create-base.sql
@@ -140,6 +140,44 @@ CREATE TABLE experiment_variants (
 CREATE INDEX idx_variants_experiment
   ON experiment_variants (experiment_id);
 
+
+-- ============================
+-- INVOICES
+-- ============================
+
+CREATE TABLE invoice (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  email VARCHAR(255) NOT NULL,
+  stripe_id VARCHAR(255) NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+-- ============================
+-- INVOICES UPDATE RULE
+-- ============================
+
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_invoice_updated_at
+BEFORE UPDATE ON invoice
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at();
+
+-- ============================
+-- INVOICES INDEXES
+-- ============================
+
+CREATE INDEX idx_invoice_email ON invoice(email);
+CREATE INDEX idx_invoice_stripe_id ON invoice (stripe_id);
+
 -- ============================
 -- ROW LEVEL SECURITY (RLS)
 -- ============================
