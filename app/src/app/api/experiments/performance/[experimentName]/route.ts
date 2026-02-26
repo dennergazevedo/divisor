61873ddf0d267bcb86bdc754f386b017ab56a43b
Denner Azevedo <dennergazevedo@gmail.com>
2026-02-25 22:24:26 -0300

    fix: promise GET param

--- app/src/app/api/experiments/performance/[experimentName]/route.ts
+++ app/src/app/api/experiments/performance/[experimentName]/route.ts
@@ -3,27 +3,33 @@ import { getExperimentPerformance } from "@/services/bigquery";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { experimentName: string } },
+  context: { params: Promise<{ experimentName: string }> }
 ) {
   try {
+    const { experimentName } = await context.params;
+
     const { searchParams } = new URL(req.url);
     const tenantId = searchParams.get("tenantId");
-    const { experimentName } = await params;
 
     if (!tenantId) {
-      return NextResponse.json({ error: "Missing tenantId" }, { status: 400 });
+      return NextResponse.json(
+        { error: "Missing tenantId" },
+        { status: 400 }
+      );
     }
 
     const performance = await getExperimentPerformance(
       tenantId,
-      experimentName,
+      experimentName
     );
+
     return NextResponse.json({ performance });
   } catch (error) {
     console.error("Error fetching experiment performance:", error);
+
     return NextResponse.json(
       { error: "Internal Server Error" },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
